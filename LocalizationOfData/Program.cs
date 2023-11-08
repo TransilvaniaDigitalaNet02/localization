90f77441c5b789db2d77bab46ae02d2c73feda80
Florin Lazar <florin.lazar.it2014@gmail.com>
2023-11-08 23:14:20 +0200

    localization of data - some samples

---
+++ LocalizationOfData/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+namespace LocalizationOfData
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("ro-RO");
+            // Thread.CurrentThread.CurrentCulture = new CultureInfo("fa");
+
+            Console.Write("Please enter date=");
+            string input = Console.ReadLine();
+
+            DateTime inputDate =  Convert.ToDateTime(input, CultureInfo.CurrentCulture);
+            Console.WriteLine(inputDate);
+
+            DateTime date = new DateTime(2023, 11, 7);
+            Console.WriteLine(date);
+
+            decimal discount = -100;
+            Console.WriteLine(discount.ToString("C"));
+        }
+    }
+}
